9b427efbaef92317e5d1ac5ff714aaeee95712d0
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2024-11-12 17:23:51 -0400

    chore: add base api config and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger(bootstrap.name);
+
+  // Config
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('movie-ai-explorer')
+    .setDescription('description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+  logger.log(`\n🚀 Server Initialized on Port: ${process.env.PORT}\n`);
 }
 bootstrap();
